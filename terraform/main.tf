149f8a715db73a65665d25cf5c9a19d7c5bda151
amanuel <amanuelchukala@gmail.com>
2025-03-27 11:44:18 -0600

    Add Redshift Serverless configuration and outputs to Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,3 +27,55 @@ resource "aws_kinesis_stream" "data_stream" {
     Environment = "Dev"
   }
 }
+
+# Redshift Serverless Namespace
+resource "aws_redshiftserverless_namespace" "etl_namespace" {
+  namespace_name = var.redshift_namespace
+  admin_username = var.redshift_username
+  admin_user_password = var.redshift_password
+}
+
+# Redshift Serverless Workgroup
+resource "aws_redshiftserverless_workgroup" "etl_workgroup" {
+  workgroup_name = var.redshift_workgroup
+  namespace_name = aws_redshiftserverless_namespace.etl_namespace.namespace_name
+  publicly_accessible = true
+
+  tags = {
+    Name        = "ETL Redshift Workgroup"
+    Environment = "Dev"
+  }
+}
+
+# Redshift Security Group (Allow Access from anywhere for demo purposes)
+resource "aws_security_group" "redshift_sg" {
+  name        = "redshift_sg"
+  description = "Allow Redshift access"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 5439
+    to_port     = 5439
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "redshift-sg"
+  }
+}
+
+# Execute SQL script to create Redshift tables
+resource "aws_redshiftdata_statement" "create_redshift_tables" {
+  database   = "dev"
+  sql        = file("${path.module}/../redshift/create_tables.sql")
+  workgroup_name = aws_redshiftserverless_workgroup.etl_workgroup.workgroup_name
+  depends_on = [aws_redshiftserverless_workgroup.etl_workgroup]
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,19 @@ output "s3_bucket_name" {
 output "kinesis_stream_name" {
   value = aws_kinesis_stream.data_stream.name
 }
+
+output "redshift_namespace" {
+  value = aws_redshiftserverless_namespace.etl_namespace.namespace_name
+}
+
+output "redshift_workgroup_name" {
+  value = aws_redshiftserverless_workgroup.etl_workgroup.workgroup_name
+}
+
+output "redshift_endpoint" {
+  value = aws_redshiftserverless_workgroup.etl_workgroup.endpoint
+}
+
+output "redshift_database_url" {
+  value = "jdbc:redshift://${aws_redshiftserverless_workgroup.etl_workgroup.endpoint}/dev"
+}
