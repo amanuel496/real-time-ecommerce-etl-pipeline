40e7ac0eeaf905e5e9001262c7d5ccebd9153476
amanuel <amanuelchukala@gmail.com>
2025-04-01 00:53:50 -0600

    Add .gitignore and update Terraform variables for Redshift configuration

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,3 +13,29 @@ variable "kinesis_stream_name" {
   type        = string
   description = "Kinesis stream for real-time ingestion"
 }
+
+variable "redshift_namespace" {
+  type        = string
+  description = "Redshift Serverless namespace"
+}
+
+variable "redshift_workgroup" {
+  type        = string
+  description = "Redshift Serverless workgroup"
+}
+
+variable "redshift_username" {
+  type        = string
+  description = "Redshift admin username"
+}
+
+variable "redshift_password" {
+  type        = string
+  description = "Redshift admin password"
+  sensitive   = true
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID for Redshift security group"
+}
